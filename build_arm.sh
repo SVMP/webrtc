fb3bc287933ecebf4a2cc0838e562aa873f70a50
Andy Pyles <apyles@mitre.org>
2013-08-01 11:44:26 -0400

    add arm build script.

---
+++ build_arm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#gclient sync --nohooks
+#export PATH=$PATH:../depot_tools
+export PATH=$PATH:/home/apyles/android-sdk-linux/platform-tools:/home/apyles/depot_tools:/usr/local/bin
+export WEBRTC_SKIP_RESOURCES_DOWNLOAD=1
+. build/android/envsetup.sh 
+GYP_GENERATOR_OUTPUT="out_arm" gclient runhooks --force
+
+ninja -C out_arm/out/Debug -j 4  All
+#ninja -C out/Release -j 4  All
