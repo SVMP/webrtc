7b6abe8c81648408506656021e62909db9cc64db
Andy Pyles <apyles@mitre.org>
2013-08-01 11:07:08 -0400

    add build scripts.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#gclient sync --nohooks
+#export PATH=$PATH:../depot_tools
+export PATH=$PATH:/home/apyles/android-sdk-linux/platform-tools:/home/apyles/depot_tools:/usr/local/bin
+export WEBRTC_SKIP_RESOURCES_DOWNLOAD=1
+. build/android/envsetup.sh --target-arch=x86
+gclient runhooks
+
+ninja -C out/Debug -j 4  All
+#ninja -C out/Release -j 4  All
---
+++ build_host.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#gclient sync --nohooks
+#export PATH=$PATH:../depot_tools
+export PATH=$PATH:/home/apyles/android-sdk-linux/platform-tools:/home/apyles/depot_tools:/usr/local/bin
+export WEBRTC_SKIP_RESOURCES_DOWNLOAD=1
+#. build/android/envsetup.sh --target-arch=x86
+GYP_GENERATOR_OUTPUT="out_linux" gclient runhooks --force
+
+ninja -C out_linux/out/Debug -j 4  peerconnection_server
+ninja -C out_linux/out/Debug -j 4  peerconnection_client
+#ninja -C out/Release -j 4  All
